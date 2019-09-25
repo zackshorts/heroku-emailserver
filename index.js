6536bb9e7af6810c40a130a7a3be841520d2ae7a
Zachary Shorts <shorts23@gmail.com>
2019-09-24 22:14:38 -0500

    trying to fix CORS issues with heroku

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ var cors = require('cors');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.get('/', (req, res) => res.send('Heroku app is running!'));
 app.post('/sendemail', (req, res) => {

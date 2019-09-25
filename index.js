512f76a4be5b0f04003a9c9537e437bbb6d44b55
Zachary Shorts <shorts23@gmail.com>
2019-09-24 23:20:42 -0500

    trying to fix CORS issues with heroku

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
 const app = express();
 const port = process.env.PORT || 3000;
-var cors = require('cors');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(cors);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type, application/x-www-form-urlencoded');
     next();
 });
 

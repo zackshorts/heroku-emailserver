d051a759a58d6d8d376f9ab49f178d9676d68602
Zachary Shorts <shorts23@gmail.com>
2019-09-25 20:09:48 -0500

    Update index.js
    trying to resolve json error

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const sgMail = require('@sendgrid/mail');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);

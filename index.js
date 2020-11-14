e2e19ecb3e5d5b29b5d8e762aa360cb0353f67e4
Zachary Shorts <shorts23@gmail.com>
2020-11-13 19:17:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type, application/x-www-form-urlencoded');
     next();
 });
 
